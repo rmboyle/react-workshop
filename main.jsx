726f648f6097f2b46e081e4c723ba804867bb09a
Richard Boyle <rmichaelboyle@gmail.com>
2015-06-29 10:02:42 -0400

    Clean up webpack config

--- main.jsx
+++ main.jsx
@@ -1,3 +1,4 @@
 var React = require('react');
-var Hello = require('./Hello');
+var Hello = require('./hello');
+
 React.render(<Hello />, document.getElementById('hello'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './index.jsx',
+  entry: './main.jsx',
   output: {
     filename: 'bundle.js', //this is the default name, so you can skip it
     //at this directory our bundle file will be available
@@ -10,15 +10,10 @@ module.exports = {
     loaders: [
       {
         //tell webpack to use jsx-loader for all *.jsx files
-        test: /\.jsx$/,
-        loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+        test: /\.js$/,
+        loader: 'jsx-loader?harmony'
       }
     ]
-  },
-  externals: {
-    //don't bundle the 'react' npm package with our bundle.js
-    //but get it from a global 'React' variable
-    'react': 'React'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
