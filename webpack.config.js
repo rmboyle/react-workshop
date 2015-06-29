65829fa8283fddc0787542c8cf504561e1c5faa2
Richard Boyle <rmichaelboyle@gmail.com>
2015-06-29 10:20:00 -0400

    Clean up config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   entry: './main.jsx',
   output: {
-    filename: 'bundle.js', //this is the default name, so you can skip it
-    //at this directory our bundle file will be available
-    //make sure port 8090 is used when launching webpack-dev-server
-    publicPath: 'http://localhost:8090/assets'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         //tell webpack to use jsx-loader for all *.jsx files
         test: /\.js$/,
-        loader: 'jsx-loader?harmony'
+        loader: 'jsx-loader?insertPragma=React.DOM&harmony'
       }
     ]
   },
